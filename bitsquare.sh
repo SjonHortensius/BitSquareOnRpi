4154260cb302b479c84b3ff56fb78cb7fb2fe219
Sjon Hortensius <sjon@hortensius.net>
2016-06-12 23:24:23 +0200

    small tweak allowing re-run to update

--- bitsquare.sh
+++ bitsquare.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-#set -e
+set -e
 
 # openjfx is available in the testing repo
-echo 'deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib non-free rpi' | sudo tee -a /etc/apt/sources.list
+echo 'deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib non-free rpi' | sudo tee -a /etc/apt/sources.list >/dev/null
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk openjfx maven tor -y
 
-mkdir -p ~/src; cd ~/src
+mkdir -p ~/src
 
 # build bitcoinj
-git clone -b FixBloomFilters https://github.com/bitsquare/bitcoinj.git
-cd bitcoinj
+cd ~/src
+[[ -d bitcoinj ]] || git clone -b FixBloomFilters https://github.com/bitsquare/bitcoinj.git
+cd bitcoinj; git pull
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true
 
 # build bitsquare
 cd ~/src
-git clone https://github.com/bitsquare/bitsquare.git
-cd bitsquare/
+[[ -d bitsquare ]] || git clone https://github.com/bitsquare/bitsquare.git
+cd bitsquare/; git pull
 
-wget https://github.com/SjonHortensius/bitsquare/commit/338f7f117939ecc2fc302c3d57079f6c81c851a7.patch
-patch -p1 <338f7f117939ecc2fc302c3d57079f6c81c851a7.patch 
+wget https://github.com/SjonHortensius/bitsquare/commit/338f7f117939ecc2fc302c3d57079f6c81c851a7.patch || :
+patch -p1 <338f7f117939ecc2fc302c3d57079f6c81c851a7.patch || :
 
 mvn clean package -DskipTests
 
-echo -e 'Please run this command to continue:\n\t/usr/bin/java -jar ~/src/bitsquare/gui/target/shaded.jar'
+echo -e 'Run this script again to update from github. Run this command to start Bitsquare:\n\t/usr/bin/java -jar ~/src/bitsquare/gui/target/shaded.jar'
